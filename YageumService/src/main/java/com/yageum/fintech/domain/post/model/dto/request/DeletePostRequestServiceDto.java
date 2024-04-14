44b15f6a053f79b57ff6abbfb79b548221508535
RyuChaeHyun <lch010201@gmail.com>
2024-04-14 23:51:30 +0900

    refactor: 불필요 코드 제거

--- YageumService/src/main/java/com/yageum/fintech/domain/post/model/dto/request/DeletePostRequestServiceDto.java
+++
@@ -1,7 +0,0 @@
-package com.yageum.fintech.domain.post.model.dto.request;
-
-public record DeletePostRequestServiceDto(
-	Long projectId,
-	Long postId
-) {
-}
--- YageumService/src/main/java/com/yageum/fintech/domain/post/model/dto/request/RetrieveDetailPostRequestServiceDto.java
+++
@@ -1,7 +0,0 @@
-package com.yageum.fintech.domain.post.model.dto.request;
-
-public record RetrieveDetailPostRequestServiceDto(
-	Long projectId,
-	Long postId
-) {
-}
