31f5724e4672c8324b455e72b8da82f77c49939a
RyuChaeHyun <lch010201@gmail.com>
2024-04-19 15:38:01 +0900

    refactor: 예외 리스트 수정

--- YageumService/src/main/java/com/yageum/fintech/global/model/Exception/ExceptionList.java
+++ YageumService/src/main/java/com/yageum/fintech/global/model/Exception/ExceptionList.java
@@ -2,17 +2,13 @@ package com.yageum.fintech.global.model.Exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Getter
 public enum ExceptionList {
 
     UNKNOWN(-9999, "알 수 없는 오류가 발생하였습니다."),
-
-    //Tenant, Lessor
-    NON_EXISTENT_CHECKLIST(5005, "내용이 존재하지 않습니다."),
-    INVALID_USER_ID(5011, "요청으로 들어온 User의 식별자가 유효하지 않습니다."),
+    INVALID_INPUT_VALUE(5009, "유효성 검사에 실패하였습니다."),
 
     BAD_REQUEST(400, "잘못된 요청입니다"),
     TOKEN_IS_NOT_SAME(401, "액세스 토큰과 리프레시 토큰이 일치하지 않습니다"),
@@ -33,41 +29,24 @@ public enum ExceptionList {
     ILLEGAL_ARGUMENT_JWT(7006, "잘못된 인자가 전달되었습니다."),
     SECURITY_JWT(7007, "보안 관련 문제로 토큰 검증이 실패했습니다."),
 
-    // SCHEDULE
-    NON_EXISTENT_SCHEDULE(6000, "일정이 존재하지 않습니다."),
-    NON_EXISTENT_SCHEDULELIST(6001, "일정 목록이 존재하지 않습니다."),
-    START_DATE_AFTER_END_DATE_EXCEPTION(6002, "시작일이 종료일의 이후입니다."),
-    INVALID_DATE_FORMAT(6003, "유효하지 않은 날짜 형태입니다."),
-    OUT_OF_PROJECT_SCHEDULE_RANGE(6004, "프로젝트의 일정 범위를 벗어났습니다."),
-
-    NON_EXISTENT_PROJECT(5008, "프로젝트가 존재하지 않습니다."),
-    INVALID_INPUT_VALUE(5009, "유효성 검사에 실패하였습니다."),
-
-    // PROJECT
-    INVALID_PROJECT_ID(5010, "요청으로 들어온 Project의 식별자가 유효하지 않습니다."),
+    // TENANT
     NON_EXISTENT_TENANT(5010, "존재하지 않는 임차인입니다."),
-    NON_EXISTENT_TENANT_PROFILE(5010, "임차인 프로필이 존재하지 않습니다"),
-    NON_EXISTENT_LESSOR(5010, "존재하지 않는 임대인입니다."),
-    NON_EXISTENT_LESSOR_PROFILE(5010, "임대인 프로필이 존재하지 않습니다"),
-    TENANT_PROFILE_ALREADY_EXISTS(5010, "이미 해당 tenantId에 대한 프로필이 존재합니다."),
+    NON_EXISTENT_TENANT_PROFILE(5011, "임차인 프로필이 존재하지 않습니다"),
+    TENANT_PROFILE_ALREADY_EXISTS(5012, "이미 해당 tenantId에 대한 프로필이 존재합니다."),
+    NON_EXISTENT_TENANTLIST(5013, "임차인 목록이 존재하지 않습니다."),
 
-    // PROJECT & TEAM
-    NOT_TEAM_MEMBER(5012, "요청으로 들어온 User는 해당 Project의 팀원이 아닙니다."),
-
-    // POST
-    INVALID_POST_ID(5013, "요청으로 들어온 Post의 식별자가 유효하지 않습니다."),
-    NO_PERMISSION_TO_EDIT_POST(5014, "게시글 수정 권한이 없습니다."),
-
-    // Comment
-    NON_EXISTENT_COMMENT(5020, "존재하지 않는 댓글입니다."),
-    NON_EXISTENT_CATEGORY(5021, "존재하지 않는 카테고리입니다."),
+    //LESSOR
+    NON_EXISTENT_LESSOR(5020, "존재하지 않는 임대인입니다."),
+    NON_EXISTENT_LESSOR_PROFILE(5021, "임대인 프로필이 존재하지 않습니다"),
+    LESSOR_PROFILE_ALREADY_EXISTS(5022, "이미 해당 lessorId에 대한 프로필이 존재합니다."),
+    NON_EXISTENT_LESSORLIST(5023, "임대인 목록이 존재하지 않습니다."),
 
     //HOUSE
     NON_EXISTENT_HOUSE(5030, "존재하지 않은 매물입니다."),
     NON_EXISTENT_HOUSELIST(5031, "매물 목록이 존재하지 않습니다."),
-    NON_EXISTENT_TENANTLIST(5031, "임차 목록이 존재하지 않습니다."),
     NON_EXISTENT_HOUSEOPTION(5032, "매물 옵션이 존재하지 않습니다."),
 
+    //MATCH
     NON_EXISTENT_MATCH(5033, "관심 매물이 존재하지 않습니다."),
 
     // 권한
