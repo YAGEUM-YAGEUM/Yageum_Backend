c01a4bb21b8fe73b3e65679cb3084c352abe6054
RyuChaeHyun <lch010201@gmail.com>
2024-06-04 13:56:41 +0900

    feat: smart contract에 서명패드 서명값 저장되도록 추가

--- smart-contract/contracts/RealEstateContract.sol
+++ smart-contract/contracts/RealEstateContract.sol
@@ -2,14 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "./EnumDeclaration.sol";
-import "./Agreement.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract RealEstateContract {
+
+    struct ContractDetails {
+        uint deposit; // 보증금
+        uint rentAmount; // 월세
+        uint paymentSchedule; // 지불 일정
+        string propertyAddress; // 매물 주소
+        string specialTerms; // 특약사항
+        string lessorSignaturePad; // 임대인의 사인패드 서명 값
+        string tenantSignaturePad; // 임차인의 사인패드 서명 값
+    }
+
     // 공개 키
     address public lessorPublicKey;
     address public tenantPublicKey;
+
+    //원본 계약 정보
+    ContractDetails public contractDetails;
 
     // 계약서 내용 해시값
     bytes32 public messageHash;
@@ -20,14 +33,11 @@ contract RealEstateContract {
     // 계약 상태
     ContractStatus public contractStatus;
 
-    //해시값 - 계약서 내용 원본 값 매핑
-    mapping(bytes32 => string) private dataByHash;
-
     // 반환되는 값
-    event ContractInitiated(address indexed _lessor, uint _rentAmount, string _propertyAddress);
-    event ContractAccepted(address indexed _tenant);
+    event ContractInitiated(address indexed _contractAddress, address indexed _lessor, uint _rentAmount, string _propertyAddress);
+    event TenantSignatureUpdated(address indexed _tenant, string _tenantSignaturePad);
     event ContractSigned(address indexed _signer, bool _isTenant);
-    event ContractCompleted(address indexed _lessor, address indexed _tenant);
+    event ContractCompleted(address indexed _contractAddress, address indexed _lessor, address indexed _tenant);
     event ContractCanceled(address indexed _canceller, string _reason);
 
     constructor() {
@@ -46,53 +56,71 @@ contract RealEstateContract {
         return contractStatus;
     }
 
-    // Getter 함수를 통해 계약서 내용 해시값을 클라이언트에 제공
-    function getMessageHash() public view returns (bytes32) {
-        require(msg.sender == tenantPublicKey || msg.sender == lessorPublicKey, "Only the lessor or tenant can view");
-        return messageHash;
-    }
-
-    // 해시 값에 해당하는 매물 정보를 조회하는 함수
-    function getPropertyDataByHash(bytes32 _hash) public view returns (string memory) {
-        return dataByHash[_hash];
+    //매물 정보를 조회하는 함수
+    function getContractDetails() public view returns (uint, uint, uint, string memory, string memory) {
+        return (
+            contractDetails.deposit,
+            contractDetails.rentAmount,
+            contractDetails.paymentSchedule,
+            contractDetails.propertyAddress,
+            contractDetails.specialTerms
+        );
     }
 
     // 임대인이 매물 정보를 입력하고 계약서를 작성하여 임차인에게 보내는 함수
-    function sendContract(address _tenant, uint _rentAmount, uint _deposit, uint _paymentSchedule, string memory _propertyAddress, string memory _specialTerms) public {
+    function sendContract(address _tenant, uint _rentAmount, uint _deposit, uint _paymentSchedule, string memory _propertyAddress, string memory _specialTerms, string memory _lessorSignaturePad) public {
         require(msg.sender == lessorPublicKey, "Only the lessor can send a contract");
 
-        Agreement agreement = new Agreement(_deposit, _rentAmount, _paymentSchedule, _propertyAddress, _specialTerms);
+        contractDetails = ContractDetails({
+            deposit: _deposit,
+            rentAmount: _rentAmount,
+            paymentSchedule: _paymentSchedule,
+            propertyAddress: _propertyAddress,
+            specialTerms: _specialTerms,
+            lessorSignaturePad: _lessorSignaturePad,
+            tenantSignaturePad: ""
+        });
 
-        string memory propertyData = agreement.agreementDetails();
-        messageHash = agreement.agreementHash();
-
-        // 해시-데이터 매핑에 매물 정보 추가
-        dataByHash[messageHash] = propertyData;
+        // 계약서 내용을 해시화하여 messageHash 설정
+        messageHash = hashContractDetails(_deposit, _rentAmount, _paymentSchedule, _propertyAddress, _specialTerms, _lessorSignaturePad, "");
 
         tenantPublicKey = _tenant;
         contractStatus = ContractStatus.Initiated;
 
-        emit ContractInitiated(lessorPublicKey, _rentAmount, _propertyAddress);
+        emit ContractInitiated(address(this), lessorPublicKey, _rentAmount, _propertyAddress);
     }
 
-    // 임차인이 계약을 수락하는 함수
-    function acceptLeaseContract() public {
-        require(msg.sender == tenantPublicKey, "Only the tenant can accept the contract");
-        require(tenantPublicKey != address(0), "The contract has not been accepted yet");
+    // 임차인이 서명 패드로 한 사인값을 업데이트하는 함수
+    function updateTenantSignaturePad(string memory _tenantSignaturePad) public onlyTenantOrLessor {
+        require(msg.sender == tenantPublicKey, "Only the tenant can update the signature pad");
+
+        // 계약서 내용을 확인한 후 임차인의 사인 패드 값을 업데이트
+        contractDetails.tenantSignaturePad = _tenantSignaturePad;
+
+        // 계약서 내용을 해시화하여 messageHash를 업데이트
+        messageHash = hashContractDetails(
+            contractDetails.deposit,
+            contractDetails.rentAmount,
+            contractDetails.paymentSchedule,
+            contractDetails.propertyAddress,
+            contractDetails.specialTerms,
+            contractDetails.lessorSignaturePad,
+            contractDetails.tenantSignaturePad);
 
         contractStatus = ContractStatus.Accepted;
-        emit ContractAccepted(tenantPublicKey);
+        emit TenantSignatureUpdated(tenantPublicKey, _tenantSignaturePad);
     }
 
     // 임차인 또는 임대인이 계약을 서명하는 함수
     function signContract(bytes memory _signature) public onlyTenantOrLessor {
-        require(contractStatus == ContractStatus.Accepted, "The contract has not been accepted yet");
+        require(contractStatus == ContractStatus.Accepted, "To proceed with the contract, you need to sign on the signature pad first");
         require(messageHash != 0, "The contract has not been initialized yet");
 
-        // 계약서의 내용과 서명자에 따른 해시 값 계산
+        // 계약서의 내용과 서명자에 따른 해시 값 계산(서명값 생성)
         bytes32 hash = keccak256(abi.encodePacked(messageHash, msg.sender == tenantPublicKey));
         require(SignatureChecker.isValidSignatureNow(msg.sender, hash, _signature), "Invalid signature");
 
+        //임차인인지 아닌지 정보 포함
         emit ContractSigned(msg.sender, msg.sender == tenantPublicKey);
 
         if (msg.sender == tenantPublicKey) {
@@ -113,15 +141,12 @@ contract RealEstateContract {
     }
 
     // 계약을 완료하고 완료된 계약의 주소를 반환하는 함수
-    function completeTransaction() public returns (address) {
+    function completeTransaction() public {
         require(tenantPublicKey != address(0) && lessorPublicKey != address(0), "Both the tenant and lessor must sign the contract");
         require(contractStatus != ContractStatus.Completed, "The contract has already been completed");
 
         contractStatus = ContractStatus.Completed;
-        emit ContractCompleted(lessorPublicKey, tenantPublicKey);
-
-        return address(this);
-
+        emit ContractCompleted(address(this),lessorPublicKey, tenantPublicKey);
     }
 
     // 계약을 취소하는 함수
@@ -135,19 +160,9 @@ contract RealEstateContract {
         emit ContractCanceled(msg.sender, _reason);
     }
 
-    // 주소를 문자열로 변환하는 함수
-    function addressToString(address _addr) private pure returns(string memory) {
-        bytes32 value = bytes32(uint256(uint160(_addr)));
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(42);
-        str[0] = '0';
-        str[1] = 'x';
-        for (uint256 i = 0; i < 20; i++) {
-            str[2+i*2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3+i*2] = alphabet[uint8(value[i + 12] & 0x0f)];
-        }
-        return string(str);
+    // 계약서 내용을 해시화하여 messageHash를 설정하는 함수
+    function hashContractDetails(uint _deposit, uint _rentAmount, uint _paymentSchedule, string memory _propertyAddress, string memory _specialTerms, string memory _lessorSignaturePad, string memory _tenantSignaturePad) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_deposit, _rentAmount, _paymentSchedule, _propertyAddress, _specialTerms, _lessorSignaturePad, _tenantSignaturePad));
     }
 
 }
