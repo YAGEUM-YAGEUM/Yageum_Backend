2d6029ca738a8c026db8675cfc142a9f0b7f7e5e
RyuChaeHyun <lch010201@gmail.com>
2024-06-09 18:30:46 +0900

    build: 스마트 컨트렉트 Sepolia 배포

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -1,60 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { infuraProjectId, mnemonic } = require('./secrets.json');
+
 module.exports = {
-
-  networks: {
-     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-     },
-
-    // advanced: {
-    //   port: 8777,             // Custom port
-    //   network_id: 1342,       // Custom network
-    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    //   from: <address>,        // Account to send transactions from (default: accounts[0])
-    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-    // },
+  // networks: {
+  //   development: {
+  //    host: "127.0.0.1",     // Localhost (default: none)
+  //    port: 7545,            // Standard Ethereum port (default: none)
+  //    network_id: "*",       // Any network (default: none)
+  //   },
+  sepolia: {
+    provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraProjectId}`),
+    network_id: "11155111", // Sㅊepolia network ID
+    gas: 5500000, // Gas limit
   },
 
-//  // Set default mocha options here, use special reporters, etc.
-//  mocha: {
-//     timeout: 100000
-//  },
-//
-//  // Configure your compilers
-//  compilers: {
-//    solc: {
-//      version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
-//      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-//       settings: {          // See the solidity docs for advice about optimization and evmVersion
-//        optimizer: {
-//          enabled: false,
-//          runs: 200
-//        },
-//        evmVersion: "byzantium"
-//       }
-//    }
-//  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: false,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "indexeddb",
-  //     settings: {
-  //       directory: ".db"
-  //     }
-  //   }
-  // }
+  compilers: {
+    solc: {
+      version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  }
 };
