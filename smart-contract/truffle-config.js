48946ce008b49fd540b60e090fa7fbe7032f118e
RyuChaeHyun <lch010201@gmail.com>
2024-06-09 19:25:55 +0900

    fix: Sepolia 테스트 넷 배포 에러 해결

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -2,18 +2,22 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { infuraProjectId, mnemonic } = require('./secrets.json');
 
 module.exports = {
-  // networks: {
-  //   development: {
-  //    host: "127.0.0.1",     // Localhost (default: none)
-  //    port: 7545,            // Standard Ethereum port (default: none)
-  //    network_id: "*",       // Any network (default: none)
-  //   },
+   networks: {
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
   sepolia: {
     provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraProjectId}`),
     network_id: "11155111", // Sㅊepolia network ID
     gas: 5500000, // Gas limit
+    confirmations: 2, // Number of confirmations to wait between deployments
+    skipDryRun: true,
+    pollingInterval : 1800000,
+    disableConfirmationListener: true
   },
-
+},
   compilers: {
     solc: {
       version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
