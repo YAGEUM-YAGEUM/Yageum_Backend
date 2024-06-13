6852fde5aaac8d3fb69c188b4e7dd1ed170369bf
RyuChaeHyun <lch010201@gmail.com>
2024-06-13 10:37:52 +0900

    fix: 세폴리아 배포 시 too many request 에러 해결

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -2,20 +2,22 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { infuraProjectId, mnemonic } = require('./secrets.json');
 
 module.exports = {
+   defaultNetwork: "sepolia",
    networks: {
-     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-     },
+//     development: {
+//      host: "127.0.0.1",     // Localhost (default: none)
+//      port: 7545,            // Standard Ethereum port (default: none)
+//      network_id: "*",       // Any network (default: none)
+//     },
   sepolia: {
     provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraProjectId}`),
-    network_id: "11155111", // Sㅊepolia network ID
+    network_id: "11155111", // Sepolia network ID
     gas: 5500000, // Gas limit
     confirmations: 2, // Number of confirmations to wait between deployments
     skipDryRun: true,
     pollingInterval : 1800000,
-    disableConfirmationListener: true
+    disableConfirmationListener: true,
+    timeoutBlocks: 200,
   },
 },
   compilers: {
